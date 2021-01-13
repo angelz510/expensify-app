6d116fd58e14d499dbd8f36c2e4af73461be65cb
Angel R <angelzrod4@gmail.com>
2021-01-12 18:21:39 -0800

    added routes

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = (props) => (
+  <div>
+    This is the EditExpensePage.
+  </div>
+);
+
+export default EditExpensePage;
---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExpenseDashboardPage = () => (
+  <div>
+    This is the ExpenseDashboardPage.
+  </div>
+);
+
+export default ExpenseDashboardPage;

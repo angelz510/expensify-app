7f37e76d26aa117323a6c1f02613e0956449093b
Angel R <angelzrod4@gmail.com>
2021-03-15 11:24:10 -0700

    able to create into firebase from redux

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,6 +14,27 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-firebase.database().ref().set({
-  name: 'Angel R'
-});
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref('expense').push({
+//   description: 'Puzzle',
+//   note: '',
+//   amount: 12.99,
+//   createdAt: 'Monday'
+// });
+
+// database.ref('expense').push({
+//   description: 'Candle',
+//   note: '',
+//   amount: 5.99,
+//   createdAt: 'Tuesday'
+// });
+
+// database.ref('expense').push({
+//   description: 'Computer',
+//   note: '',
+//   amount: 1999.99,
+//   createdAt: 'Thursday'
+// });

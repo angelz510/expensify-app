5889fdc62eb318634f8535d5e69467c76d04d21d
Angel R <angelzrod4@gmail.com>
2021-03-17 10:58:06 -0700

    moved stuff

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,12 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtA1B10k8_712zGH2WOIPd8HgAnKvvsCE",
-  authDomain: "budget-pls.firebaseapp.com",
-  databaseURL: "https://budget-pls-default-rtdb.firebaseio.com",
-  projectId: "budget-pls",
-  storageBucket: "budget-pls.appspot.com",
-  messagingSenderId: "1018378637813",
-  appId: "1:1018378637813:web:93c1e9b2ea6a58d34450bc",
-  measurementId: "G-ZLRJHXF8C5"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase

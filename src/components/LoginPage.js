612211bb6f5d6446a57753b7993fd985e4ee95b5
Angel R <angelzrod4@gmail.com>
2021-03-24 13:21:09 -0700

    styled header and summary area

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,7 +7,7 @@ export const LoginPage = ({ startLogin }) => (
     <div className="box-layout__box">
       <h1 className="box-layout__title">Budget Pls</h1>
       <p>Let's organize your expenses.</p>
-      <button className="button__login" onClick={startLogin}>Login with Google</button>
+      <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
     
   </div>

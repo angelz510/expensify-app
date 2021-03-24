9001b3a1cde135ee3508748e75f2c4c99424d280
Angel R <angelzrod4@gmail.com>
2021-03-24 16:17:07 -0700

    styled items and added loader

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoadingPage = () => (
+  <div className="loader">
+    <img className="loader_img" src="/images/loader.gif"/>
+  </div>
+);
+
+export default LoadingPage;
